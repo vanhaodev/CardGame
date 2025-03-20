cf49e9ca5cdc1c9963645ba47dab3fcd12b4cd8d
BabyMonster <vanhao.dev@gmail.com>
2025-03-20 19:08:19 +0700

    add

--- Client/Assets/Functions/System/Startup/HomeStartup.cs
+++ Client/Assets/Functions/System/Startup/HomeStartup.cs
@@ -9,8 +9,14 @@ namespace Globals
         protected override async void Awake()
         {
             base.Awake();
+#if (UNITY_ANDROID || UNITY_IOS) && !UNITY_EDITOR
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
             Application.targetFrameRate = 60;
-            await UniTask.WaitUntil(()=> Global.Instance != null);
+#endif
+#if UNITY_EDITOR
+            Application.targetFrameRate = -1;
+#endif
+            await UniTask.WaitUntil(() => Global.Instance != null);
             await Global.Instance.Init();
             await Global.Instance.WaitForInit<SoundManager>();
             Global.Instance.Get<SoundManager>().PlaySoundLoop(5, 1);
