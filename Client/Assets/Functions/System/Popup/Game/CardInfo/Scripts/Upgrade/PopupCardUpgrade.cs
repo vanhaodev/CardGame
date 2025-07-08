972069abdc7024f9364b023eb815e21118374511
BabyMonster <vanhao.dev@gmail.com>
2025-07-08 17:23:31 +0700

    add popup card upgrade

---
+++ Client/Assets/Functions/System/Popup/Game/CardInfo/Scripts/Upgrade/PopupCardUpgrade.cs
@@ -0,0 +1,16 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Utils.Tab;
+
+public class PopupCardUpgrade :  MonoBehaviour, ITabSwitcherWindow
+{
+    public async UniTask Init(TabSwitcherWindowModel model = null)
+    {
+
+    }
+
+    public async UniTask LateInit()
+    {
+        
+    }
+}
